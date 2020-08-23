d75b54fe8efe00ef62b892a5673a0251e3a59ee6
turj00 <turjo1331@gmail.com>
2020-08-24 05:54:09 +0600

    Updates

---
+++ precache-manifest.aaeb9189f6aab5dc43b4f93bcbb7f4e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840ef6cd5895da50527683af2a0e9202",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "268ecc791b3348c06022",
+    "url": "/Todo-list/static/js/2.e63eb72b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo-list/static/js/2.e63eb72b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89629828d8d750359dc1",
+    "url": "/Todo-list/static/js/main.acb6bd42.chunk.js"
+  },
+  {
+    "revision": "70abc1a708de31e67c89",
+    "url": "/Todo-list/static/js/runtime-main.02407e25.js"
+  }
+]);
